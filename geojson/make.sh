49aa5b6706c439ea9e402543067bcccb8ad1c055
tmiyachi <t.miyachi@hotmail.co.jp>
2023-04-07 22:21:46 +0900

    パイプによる入力が失敗するため修正

--- geojson/make.sh
+++ geojson/make.sh
@@ -6,7 +6,10 @@
 # - 簡素化と小さなポリゴンの除去を行いファイルサイズを小さくする
 
 # directories settings
-SCRIPTDIR=$(cd $(dirname $0); pwd)
+SCRIPTDIR=$(
+  cd $(dirname $0)
+  pwd
+)
 GEODIR=${SCRIPTDIR}
 MSCMD="npx mapshaper"
 JMADIR=${SCRIPTDIR}/../jma
@@ -19,7 +22,7 @@ SIMPLIFY_MINAREA="1000m2"
 JMASHP=${JMADIR}/AreaInformationCity_weather_GIS.shp
 JMACODE=${CODEDIR}/jmacode.csv
 
-rm ${GEODIR}/*.geojson
+rm -f ${GEODIR}/*.geojson
 
 # geojsonに変換
 echo -e "\e[1;33mConvert shape to geoJSON...\e[0;m"
@@ -27,14 +30,14 @@ $MSCMD -i ${JMASHP} encoding="utf-8" -filter-fields regioncode -clean -o ${GEODI
 
 # 簡素化
 echo -e "\e[1;33mSimplify geoJSON...\e[0;m"
-$MSCMD ${GEODIR}/tmp.geojson -simplify ${SIMPLIFY_PERCENT} -o - | \
-$MSCMD -i - -filter-slivers min-area=${SIMPLIFY_MINAREA} -o - | \
-$MSCMD -i - -clean -o ${GEODIR}/tmp.geojson force
+$MSCMD ${GEODIR}/tmp.geojson -simplify ${SIMPLIFY_PERCENT} -o ${GEODIR}/tmp.geojson force
+$MSCMD -i ${GEODIR}/tmp.geojson -filter-slivers min-area=${SIMPLIFY_MINAREA} -o ${GEODIR}/tmp.geojson force
+$MSCMD -i ${GEODIR}/tmp.geojson -clean -o ${GEODIR}/tmp.geojson force
 
 # フィールドを追加
 echo -e "\e[1;33mJoin xml code table to fields...\e[0;m"
-$MSCMD ${GEODIR}/tmp.geojson -each 'citycode=regioncode, delete regioncode' -o - | \
-$MSCMD -i - -join ${JMACODE} keys=citycode,citycode string-fields=* -o ${GEODIR}/city.geojson force
+$MSCMD ${GEODIR}/tmp.geojson -each 'citycode=regioncode, delete regioncode' -o ${GEODIR}/tmp.geojson force
+$MSCMD ${GEODIR}/tmp.geojson -join ${JMACODE} keys=citycode,citycode string-fields=* -o ${GEODIR}/city.geojson force
 
 # コードレベルでまとめる
 echo -e "\e[1;33mAggregate groups of features using a code field...\e[0;m"
@@ -42,4 +45,4 @@ $MSCMD ${GEODIR}/city.geojson -dissolve matomeareacode copy-fields=prefcode,pref
 $MSCMD ${GEODIR}/city.geojson -dissolve firstareacode copy-fields=prefcode,prefname,firsareaneme -o ${GEODIR}/firstarea.geojson force
 $MSCMD ${GEODIR}/city.geojson -dissolve prefcode copy-fields=prefname -o ${GEODIR}/pref.geojson force
 
-rm ${GEODIR}/tmp.geojson
+#rm ${GEODIR}/tmp.geojson
